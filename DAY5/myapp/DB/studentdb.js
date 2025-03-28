39bcc60693927a983a18734afe551bc29c2eef6e
MohamadNafrinShafni <nafrinn03@gmail.com>
2025-03-28 13:01:59 +0530

    created DAY5 Folder with js files

---
+++ DAY5/myapp/DB/studentdb.js
@@ -0,0 +1,15 @@
+//array of student JSON details
+
+let students = [
+    {regno:'2021ict01',name:'Shafni',gender:'F'},
+    {regno:'2021ict02',name:'Nafrin',gender:'M'},
+    {regno:'2021ict02',name:'Athni',gender:'M'},
+    {regno:'2021ict03',name:'Zakky',gender:'M'},
+    {regno:'2021ict04',name:'Zuha',gender:'F'},
+    {regno:'2021ict05',name:'Ziba',gender:'F'},
+    {regno:'2021ict06',name:'Arfin',gender:'M'},
+    {regno:'2021ict07',name:'Airis',gender:'F'},
+    {regno:'2021ict08',name:'Nathasha',gender:'M'}
+    ];
+
+    module.exports=students;
